c017254d67040e1b6d7541938318e7003b361a9f
Ayoub Moustahfid <moustahfidayoub12@gmail.com>
2021-04-28 21:18:40 +0000

    push deux component register and validation account

--- Routes.js
+++ Routes.js
@@ -1,15 +1,16 @@
 import React from 'react'
-import {View} from 'react-native'
 import {NativeRouter, Switch, Route, Redirect} from 'react-router-native'
 
 // Import All Component
 import Register from './component/Register'
+import ActivationAccount from './component/ActivationAccount'
 
 export default function Routes() {
     return (
         <NativeRouter>
           <Switch>
             <Route path="/" exact component={Register} />
+            <Route path="/active-account" exact component={ActivationAccount} />
           </Switch>  
         </NativeRouter>
     )
